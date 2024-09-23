e9b982e548e47c6c7d917e19cc4c22de9e6d5de9
Snegov1k <Alekseevich.m.k@gmail.com>
2024-09-23 19:04:28 +0300

    total nolit of the s3 package

--- internal/repository/s3/s3.go
+++ internal/repository/s3/s3.go
@@ -1,4 +1,5 @@
-package s3 //nolint:typecheck,nolintlint
+// Package s3 nolint
+package s3
 
 import (
 	"avatar_service/internal/config"
@@ -9,7 +10,7 @@ import (
 	_ "image/jpeg" // Регистрация формата jpeg для декодирования изображения в convertToWebP()
 	_ "image/png"  // Регистрация формата png для декодирования изображения в convertToWebP()
 
-	"github.com/kolesa-team/go-webp/encoder" //nolint:typecheck,nolintlint
+	"github.com/kolesa-team/go-webp/encoder"
 	"github.com/kolesa-team/go-webp/webp"
 	"github.com/minio/minio-go/v7"
 	"github.com/minio/minio-go/v7/pkg/credentials"
