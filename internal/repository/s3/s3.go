bf1239d46e269e610031399962c17220126de90f
Snegov1k <Alekseevich.m.k@gmail.com>
2024-09-24 09:16:34 +0300

    attempt to make linter ignore the package

--- internal/repository/s3/s3.go
+++ internal/repository/s3/s3.go
@@ -1,4 +1,4 @@
-package s3
+package s3 //nolint:typecheck
 
 import (
 	"avatar_service/internal/config"
@@ -9,7 +9,7 @@ import (
 	_ "image/jpeg" // Регистрация формата jpeg для декодирования изображения в convertToWebP()
 	_ "image/png"  // Регистрация формата png для декодирования изображения в convertToWebP()
 
-	"github.com/kolesa-team/go-webp/encoder" //nolint:typecheck
+	"github.com/kolesa-team/go-webp/encoder"
 	"github.com/kolesa-team/go-webp/webp"
 	"github.com/minio/minio-go/v7"
 	"github.com/minio/minio-go/v7/pkg/credentials"
